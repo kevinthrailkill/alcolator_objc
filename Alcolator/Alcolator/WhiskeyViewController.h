e5382eeda09bff220a10d7adb7dbd8350963fcf0
Kevin Thrailkill <kevinjt92@gmail.com>
2016-05-06 16:14:53 -0700

    Alcolator - Storyboard connect to code completed

---
+++ Alcolator/Alcolator/WhiskeyViewController.h
@@ -0,0 +1,13 @@
+//
+//  WhiskeyViewController.h
+//  Alcolator
+//
+//  Created by Kevin Thrailkill on 5/6/16.
+//  Copyright © 2016 kevinthrailkill. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface WhiskeyViewController : ViewController
+
+@end
